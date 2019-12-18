48fe7a109b71bb8405e324a298e6cb5951ec50af
mirelamocanu <mirela.mocanu1984@gmail.com>
2019-12-18 19:45:04 +0200

    updated class for test

--- IntroToJava/src/com/tc/test/MainClass.java
+++ IntroToJava/src/com/tc/test/MainClass.java
@@ -3,7 +3,7 @@ package com.tc.test;
 public class MainClass {
 
     public static void main(String[] args) {
-        System.out.println("This is just for check");
+        System.out.println("This is just for check after first push");
     }
 
 }
